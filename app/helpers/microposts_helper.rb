a849320815195fdf46c60353be465ee69ff1d0ef
Tanner Welsh <tanner@openspectrumstudios.com>
2012-01-22 10:56:31 -0500

    Fixed text wrapping for microposts

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -8,9 +8,9 @@ module MicropostsHelper
   private
 
   def wrap_long_string(text, max_width = 30)
-    zero_width_space = "&#8203;"
-    regex = /.{1,#{max.width}}/
-    (text.length < max_width) ? text : text.scan(regex).join(zero_width_space)
+    connector = "-"
+    regex = /.{1,#{max_width}}/
+    (text.length < max_width) ? text : text.scan(regex).join(connector)
   end
  
     
